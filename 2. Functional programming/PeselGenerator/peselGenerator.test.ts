fc772db7f299a52903badd4736b07b85c9e42ddf
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2022-07-23 13:10:57 +0200

    Create tests for pesel generator

--- 2. Functional programming/PeselGenerator/peselGenerator.test.ts
+++ 2. Functional programming/PeselGenerator/peselGenerator.test.ts
@@ -1,9 +1,30 @@
 import { generatePesel } from "./peselGenerator";
+import { checkPesel } from "./utils/utils";
 
 describe("Testing pesel generator", () => {
   describe("Given date of birth", () => {
+    const firstValidDateOfBirth = "2012/08/12";
+    const secondValidDateOfBirth = "1800/12/03";
+    const invalidDateOfBirth = "1799/12/02";
+
     describe("When no gender specified", () => {
-      test("Then I should get valid ");
+      test("Then I should get valid pesel", () => {
+        const firstPesel = generatePesel(firstValidDateOfBirth);
+        const secondPesel = generatePesel(secondValidDateOfBirth);
+
+        expect(checkPesel(firstPesel)).toBe(true);
+        expect(checkPesel(secondPesel)).toBe(true);
+        expect(firstPesel.slice(0, 6)).toBe("120812");
+      });
+    });
+    describe("When male specified", () => {
+      test("Then I should get valid pesel with 10th digit being odd", () => {});
+    });
+    describe("When female specified", () => {
+      test("Then I should get valid pesel with 10th digit being even", () => {});
+    });
+    describe("When birth date is from before 1800 or after 2499", () => {
+      test("Then I should have error thrown with message 'Wrong date!'", () => {});
     });
   });
 });
