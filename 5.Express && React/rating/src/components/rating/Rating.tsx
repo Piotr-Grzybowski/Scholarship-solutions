15feb248b233c2a71d6422e42dd06c6caa19cba9
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2022-09-25 18:18:47 +0200

    Add styling with mui

--- 5.Express && React/rating/src/components/rating/Rating.tsx
+++ 5.Express && React/rating/src/components/rating/Rating.tsx
@@ -1,16 +1,24 @@
 import Score from "../score/Score";
 import { rating } from "../types";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 function Rating({ name, score, content }: rating) {
   return (
-    <div>
-      <span>Name: {name}</span>
-      <span>
-        Score: <Score score={score}></Score>
-      </span>
-      <span>Description: {content.substring(0, 120)}...</span>
-      <hr></hr>
-    </div>
+    <ListItem sx={{ display: "flex", justifyContent: "space-evenly" }}>
+      <ListItemIcon sx={{ marginRight: "10px", maxWidth: "25%" }}>
+        <Score score={score}></Score>
+      </ListItemIcon>
+      <ListItemText
+        primary={name}
+        sx={{ marginRight: "5%", maxWidth: "10%" }}
+      />
+      <ListItemText
+        primary={content.substring(0, 120) + "..."}
+        sx={{ maxWidth: "60%", textAlign: "justify" }}
+      />
+    </ListItem>
   );
 }
 
--- 5.Express && React/rating/src/components/ratingslist/RatingsList.tsx
+++ 5.Express && React/rating/src/components/ratingslist/RatingsList.tsx
@@ -1,16 +1,17 @@
 import Rating from "../rating/Rating";
 import AverageScore from "../averagescore/AverageScore";
 import { ratings } from "../types";
+import List from "@mui/material/List";
 
 function RatingsList({ ratings }: ratings) {
   const tenFirstRatings = ratings.length > 10 ? ratings.slice(0, 10) : ratings;
   return (
-    <div>
+    <List sx={{ width: "100%", maxWidth: 700 }}>
       <AverageScore ratings={ratings}></AverageScore>
       {tenFirstRatings.map((element, index) => {
         return <Rating {...element} key={index}></Rating>;
       })}
-    </div>
+    </List>
   );
 }
 
